02710855f83725e9aefd5d511a869961dd9154e4
Ewerson Castelo <ewerson.castelo@gmail.com>
2022-04-16 17:43:36 -0400

    chore: initial commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Ewerson on 16/04/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
