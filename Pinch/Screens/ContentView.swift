fd4f2431a9c24ed779c5392306885ffc306919d5
Ewerson Castelo <ewerson.castelo@gmail.com>
2022-04-19 22:08:58 -0400

    chore: add struct for homeView

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		 NavigationView {
+			 ZStack{
+				 Image("magazine-front-cover")
+					 .resizable()
+					 .aspectRatio(contentMode: .fit)
+					 .cornerRadius(10)
+					 .padding()
+					 .shadow(color: .black.opacity(0.4), radius: 12, x: 2, y: 2)
+			 }
+			 .navigationTitle("Pinch e Zoom")
+			 .navigationBarTitleDisplayMode(.inline)
+		 }
+		 .navigationViewStyle(.stack)
     }
 }
 
