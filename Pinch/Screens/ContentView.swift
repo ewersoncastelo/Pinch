856f433d16e8d3c44fd167f0950a098179fa1d17
Ewerson Castelo <ewerson.castelo@gmail.com>
2022-04-19 22:22:41 -0400

    chore: create animation for image

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+	// MARK: - PROPERTY
+	@State private var isAnimating: Bool = false
+	
+	// MARK: - BODY
     var body: some View {
 		 NavigationView {
 			 ZStack{
@@ -17,14 +21,21 @@ struct ContentView: View {
 					 .cornerRadius(10)
 					 .padding()
 					 .shadow(color: .black.opacity(0.4), radius: 12, x: 2, y: 2)
+					 .opacity(isAnimating ? 1: 0)
 			 }
 			 .navigationTitle("Pinch e Zoom")
 			 .navigationBarTitleDisplayMode(.inline)
+			 .onAppear{
+				 withAnimation(.linear(duration: 1)){
+					 isAnimating = true
+				 }
+			 }
 		 }
 		 .navigationViewStyle(.stack)
     }
 }
 
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
