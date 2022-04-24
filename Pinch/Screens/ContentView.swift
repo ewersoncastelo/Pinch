ce0e80abfdee3f7c664ca709ccac361eb5d7fc88
Ewerson Castelo <ewerson.castelo@gmail.com>
2022-04-24 15:01:05 -0400

    chore: add onTapGesture for zoom in to image

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
 	// MARK: - PROPERTY
 	@State private var isAnimating: Bool = false
+	@State private var imageSacale: CGFloat = 1
 	
 	// MARK: - BODY
     var body: some View {
@@ -22,6 +23,18 @@ struct ContentView: View {
 					 .padding()
 					 .shadow(color: .black.opacity(0.4), radius: 12, x: 2, y: 2)
 					 .opacity(isAnimating ? 1: 0)
+					 .scaleEffect(imageSacale)
+					 .onTapGesture(count: 2) {
+						 if imageSacale == 1 {
+							 withAnimation(.spring()){
+								 imageSacale = 5
+							 }
+						 } else {
+							 withAnimation(.spring()){
+								 imageSacale = 1
+							 }
+						 }
+					 }
 			 }
 			 .navigationTitle("Pinch e Zoom")
 			 .navigationBarTitleDisplayMode(.inline)
